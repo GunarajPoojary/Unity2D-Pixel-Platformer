e7fd3328b4b948cf1252b80b4a919ffd27b026c3
Gunaraj Poojary <gunarajpoojary579@gmail.com>
2026-08-19 09:02:34 +0530

    feat: smooth camera follow using smoothdamp

--- Pixel_Platformer/Assets/_Platformer/_Scripts/Player/PlayerFollowCamera.cs
+++ Pixel_Platformer/Assets/_Platformer/_Scripts/Player/PlayerFollowCamera.cs
@@ -5,9 +5,19 @@ public class PlayerFollowCamera : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private Transform _followTarget;
+    [SerializeField] private float _smoothTime = 0.1f;
+    private Vector3 _velocity;
 
     private void LateUpdate()
     {
-        _camera.transform.position = new Vector3(_followTarget.position.x, _followTarget.position.y, _camera.transform.position.z);
+        // _camera.transform.position = new Vector3(_followTarget.position.x, _followTarget.position.y, _camera.transform.position.z);
+
+        Vector3 desiredPos = _followTarget.position;
+        desiredPos.z = _camera.transform.position.z;
+
+        Vector3 smoothedPos = Vector3.SmoothDamp(
+            transform.position, desiredPos, ref _velocity, _smoothTime);
+
+        transform.position = smoothedPos;
     }
 }
